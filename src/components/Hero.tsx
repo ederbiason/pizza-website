f5063c8e9d56dbc2b0f33e1b1debbc0dc56b1fa1
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-12 19:40:06 -0300

    component: Hero, right side

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
 import css from '../styles/Hero.module.css'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 import Cherry from '../assets/Cherry.png'
+import HeroImage from '../assets/HeroImage.png'
+import Pizza1 from '../assets/p1.jpg'
+import { Phone } from 'phosphor-react'
 
 export const Hero = () => {
     return (
         <div className={css.container}>
-
             <div className={css.leftSide}>
                 <div className={css.cherryDiv}>
                     <span>
@@ -40,7 +42,35 @@ export const Hero = () => {
             </div>
 
             <div className={css.rightSide}>
-                21
+                <div className={css.imageContainer}>
+                    <Image src={HeroImage} alt="" layout='intrinsic' />
+                </div>
+
+                <div className={css.contactUs}>
+                    <span>Contact us</span>
+
+                    <div>
+                        <Phone color="white" size={28} />
+                    </div>
+                </div>
+
+                <div className={css.pizzaNotification}>
+                    <div>
+                        <Image 
+                            src={Pizza1} 
+                            alt="an image of a pizza" 
+                            objectFit='cover' 
+                            layout='intrinsic' 
+                        /> 
+                    </div>
+
+                    <div className={css.details}>
+                        <span>Italian Pizza</span>
+                        <span>
+                            <span style={{color: 'var(--themeRed)'}}>$</span> 7.49    
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
