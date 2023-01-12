31bc7d7e4f8eb30806f1d5602d27f39466ee31c3
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-12 18:06:18 -0300

    component: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,25 @@
-
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
+import { FacebookLogo, InstagramLogo, GithubLogo } from 'phosphor-react'
+import css from '../styles/Footer.module.css'
 
 export const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={css.container}>
+            <span>
+                ALL RIGHT RESERVED
+            </span>
+
+            <div className={css.social}>
+                <FacebookLogo size={40} color="#F54748" />
+                <GithubLogo size={40} color="#F54748" />
+                <InstagramLogo size={40} color="#F54748" />
+            </div>
+
+            <div className={css.logo}>
+                <Image src={Logo} alt="logo image" width={50} height={50} />
+                <span>Fudo</span>
+            </div>
         </div>
     )
 }
