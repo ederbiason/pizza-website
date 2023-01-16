552678b8f82bd108211f8bc2b39117d88667fd89
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-16 15:46:41 -0300

    fix: pizza typescript interface

--- src/@types/pizzaType.ts
+++ src/@types/pizzaType.ts
@@ -1,28 +1,31 @@
 export interface PizzaType {
-    _createdAt: string
-    _id: string
-    _rev: string
-    _type: string
-    _updatedAt: string
-    details: string
-    image: Image
-    name: string
-    price: number[]
-    slug: Slug
-  }
-  
-  export interface Image {
-    _type: string
-    asset: Asset
-  }
-  
-  export interface Asset {
-    _ref: string
-    _type: string
-  }
-  
-  export interface Slug {
-    _type: string
-    current: string
-  }
-  
+  pizzas: Pizza[]
+}
+
+export interface Pizza {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  details: string
+  image: Image
+  name: string
+  price: number[]
+  slug: Slug
+}
+
+export interface Image {
+  _type: string
+  asset: Asset
+}
+
+export interface Asset {
+  _ref: string
+  _type: string
+}
+
+export interface Slug {
+  _type: string
+  current: string
+}
